8bec2998719e1d507233461fe2248714ccadba20
Abhinav Kumar <abhinavkumar34002@gmail.com>
2024-03-07 18:52:13 +0530

    minor

--- src/lib/components/ui/dottedline.svelte
+++ src/lib/components/ui/dottedline.svelte
@@ -15,19 +15,26 @@
   let circle = heights[line].circle;
 </script>
 
-<div class=" w-2 flex flex-col items-center gap-1">
-  <div class="flex flex-col items-center">
-    {#if line !== "large"}
-      <ArrowUp />
-    {/if}
-    <div
-      class="h-[{h / 2}px] border-l-4 w-0 border-dotted border-slate-300"
-    ></div>
+{#if line === "large"}
+  <div class=" w-2 flex flex-col items-center gap-1">
+    <div class="flex flex-col items-center">
+      {#if line !== "large"}
+        <ArrowUp />
+      {/if}
+      <div
+        class="h-[125px] border-l-4 w-0 border-dotted border-slate-300"
+      ></div>
+    </div>
+    <div class="w-4 h-4 border-4 rounded-full border-[#0066FF]"></div>
+    <div class="h-[125px] border-l-4 w-0 border-dotted border-slate-300"></div>
   </div>
-  <div
-    class="w-{circle} h-{circle} border-{circle} rounded-full border-[#0066FF]"
-  ></div>
-  <div
-    class="h-[{h / 2}px] border-l-4 w-0 border-dotted border-slate-300"
-  ></div>
-</div>
+{:else}
+  <div class=" w-2 flex flex-col items-center gap-1">
+    <div class="flex flex-col items-center">
+      <ArrowUp />
+      <div class="h-[65px] border-l-4 w-0 border-dotted border-slate-300"></div>
+    </div>
+    <div class="w-8 h-8 border-8 rounded-full border-[#0066FF]"></div>
+    <div class="h-[65px] border-l-4 w-0 border-dotted border-slate-300"></div>
+  </div>
+{/if}
