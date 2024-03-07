37cd5c86f86dcb5e7b94d283cd3c9bffe98df9c5
Abhinav Kumar <abhinavkumar34002@gmail.com>
2024-03-07 14:39:26 +0530

    Completed profile section

---
+++ src/lib/components/main.svelte
@@ -0,0 +1,7 @@
+<script>
+  import Profile from "./profile.svelte";
+</script>
+
+<main class="mt-20 w-full  px-8 py-8 lg:max-w-screen-xl">
+  <Profile />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
   import Nav from "$lib/components/nav.svelte";
+  import Main from "$lib/components/main.svelte";
 </script>
 
 <div
-  class="min-h-screen w-full flex flex-col items-center justify-center bg-[#FCFCFF] font-Inter"
+  class="min-h-screen w-full flex flex-col items-center bg-[#FCFCFF] font-Inter"
 >
   <Nav />
+  <Main />
 </div>
