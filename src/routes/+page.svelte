8fab438671453613622d417ad0e93ec993105abb
Abhinav Kumar <abhinavkumar34002@gmail.com>
2024-03-07 10:49:53 +0530

    Completed nav bar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,9 @@
 <script>
+  import Nav from "$lib/components/nav.svelte";
 </script>
 
 <div
-  class="min-h-screen w-full flex flex-col items-center justify-center bg-[#FCFCFF]"
-></div>
+  class="min-h-screen w-full flex flex-col items-center justify-center bg-[#FCFCFF] font-Inter"
+>
+  <Nav />
+</div>
